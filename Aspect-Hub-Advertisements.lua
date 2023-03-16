697937c92a22834699cc16335cd01b667db42d44
DesyncDeveloper <111148565+DesyncDeveloper@users.noreply.github.com>
2023-03-16 22:32:00 +0000

    Create Aspect-Hub-Advertisements.lua

---
+++ Aspect-Hub-Advertisements.lua
@@ -0,0 +1,24 @@
+local adImages = {
+    {
+        ["Image"] = "http://www.roblox.com/asset/?id=12805366820",
+        ["Name"] = "Pet Simulator X",
+        ["Description"] = "Discover the Pet Simulator X GUI in Aspect Hub! Simply join Pet Simulator X and run our loader."
+    },
+    {
+        ["Image"] = "http://www.roblox.com/asset/?id=12805370027",
+        ["Name"] = "Bubble Gum Simulator",
+        ["Description"] = "Aspect Hub now features a Bubble Gum Simulator GUI! Join the game and execute our loader to try it out."
+    },
+    {
+        ["Image"] = "http://www.roblox.com/asset/?id=12805372414",
+        ["Name"] = "Arsenal",
+        ["Description"] = "Join Arsenal and check out the new GUI owned by Aspect Hub! Run our loader to start using it today."
+    },
+    {
+        ["Image"] = "http://www.roblox.com/asset/?id=12668203606",
+        ["Name"] = "SSS",
+        ["Description"] = "Want to duplicate items in Bubble Gum Simulator? SSS has you covered with their safe and effective method."
+    }
+}
+
+return adImages

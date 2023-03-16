4ac2312bd18db692cc7343509ec2f01a1204278c
DesyncDeveloper <111148565+DesyncDeveloper@users.noreply.github.com>
2023-03-16 15:50:30 +0000

    Update Aspect-Hub-Loading-Updates.lua

--- Aspect-Hub-Loading-Updates.lua
+++ Aspect-Hub-Loading-Updates.lua
@@ -1,1 +1,14 @@
+local updateLog = {
+    {
+        updateName = "Example Update",
+        version = "1.0",
+        description = "Added new feature X and improved feature Y."
+    },
+    {
+        updateName = "Another Update",
+        version = "1.1",
+        description = "Fixed bug Z and added option to customize colors."
+    },
+}
 
+return updateLog

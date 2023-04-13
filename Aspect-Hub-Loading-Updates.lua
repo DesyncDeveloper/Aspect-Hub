83cfe1b9659ad0ec3e218f90633ef5d98ce1dfb3
DesyncDeveloper <111148565+DesyncDeveloper@users.noreply.github.com>
2023-04-13 21:09:22 +0100

    Update Aspect-Hub-Loading-Updates.lua

--- Aspect-Hub-Loading-Updates.lua
+++ Aspect-Hub-Loading-Updates.lua
@@ -1,8 +1,23 @@
 local updateLog = {
     {
-        updateName = "Pet Simulator X | Fruit Farm",
+        updateName = "Bubble Gum Simulator | Added Fake Hatch",
         version = "1.0",
-        description = "Added new feature Fruit Farm."
+        description = "Fake Hatch has arrived, Hatch any egg and pet anywhere with a message in chat replicating the real thing!, Visual Only."
+    },
+    {
+        updateName = "Bubble Gum Simulator | Added Confirmation Screen",
+        version = "1.0",
+        description = "Confirmation Screen will display when using 'Delete All Pets','Make All Pets Shiny'"
+    },
+    {
+        updateName = "Bubble Gum Simulator | Mass Bug Fixes",
+        version = "1.0",
+        description = "Fixed many bugs, Re-coded a lot of functions"
+    },
+    {
+        updateName = "Pet Simulator X | Easter Farm",
+        version = "1.0",
+        description = "Remove fruit farm, Replaced with Easter farm."
     },
     {
         updateName = "Pet Simulator X | Comet Farm",
